f6352f2a7245d9252f64300ce3ddb63e483cc718
NoidedApe <108410620+NoidedApe@users.noreply.github.com>
2025-02-09 22:53:17 -0500

    fixed

--- assignment_3.java
+++ assignment_3.java
@@ -6,7 +6,17 @@ public class assignment_3 {
      * For numbers which are multiples of both three and five print "FizzBuzz".
      */
      public static void fizbuzz_generator() {
-
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
      }
     
     /**
@@ -15,6 +25,20 @@ public class assignment_3 {
      * For example, 0, 1, 1, 2, 3, 5, 8, etc.
     */
     public static void fibonacci_sequence() {
+        int count = 50;
+        long[] fibonacci = new long[count];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i < count; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        System.out.println("The first 50 Fibonacci numbers are:");
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci[i] + " ");
+        }
 
     }
 
@@ -26,9 +50,18 @@ public class assignment_3 {
      * @int c - third integer
      */
     public static int max_value(int a, int b, int c) {
-        
+        int highest = 0;
+        if (a > highest){
+            highest = a;
+        }
+        if(b > highest) {
+            highest = b;
+        }
+        if(c > highest){
+            highest = c;
+        }
         // Please replace this with the max integer
-        return 0;
+        return highest;
     }
     
     public static void main(String[] args) {
